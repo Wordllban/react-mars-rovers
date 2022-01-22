fc8552b0712f055c66ccdeb6f6d2a90721c8427e
KMininkov <mininkov3228@gmail.com>
2022-01-22 13:18:22 +0200

    Styles - added mars image to footer - footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,28 +1,37 @@
 import React from 'react';
+import marsPhoto from '../../assets/images/mars_footer.png'
+
 import { FooterItem } from './FooterItem';
 
 export const Footer = () => {
   return (
-    <footer className='pb-16'>
-      <div className='container mx-auto flex justify-end text-lg'>
-        <nav className='mr-32 '>
-          <h4 className='mb-4'>Navigation</h4>
-          <menu className='space-y-2'>
-            <FooterItem name="About" link="#"/>
-            <FooterItem name="Rovers" link="#"/>
-            <FooterItem name="NASA API" link="#"/>
-            <FooterItem name="Github" link="#"/>
-            <FooterItem name="Contact me" link="#"/>
-          </menu>
-        </nav>
+    <footer className='pb-20 pt-8 bg-black/30 relative'>
+      <div className='container mx-auto flex justify-around'>
         <div>
-            <h4 className='mb-4'>Social media</h4>
-            <ul className='space-y-2'>
+            <img src={marsPhoto} alt="" 
+              className='max-h-72 absolute bottom-[]'
+            />
+        </div>
+        <div className=' flex justify-end text-lg'>
+          <nav className='mr-32 '>
+            <h4 className='mb-4 text-nasa-blue'>Navigation</h4>
+            <menu className='space-y-2'>
+              <FooterItem name="About" link="#"/>
+              <FooterItem name="Rovers" link="#"/>
+              <FooterItem name="NASA API" link="#"/>
+              <FooterItem name="Github" link="#"/>
+              <FooterItem name="Contact me" link="#"/>
+          </menu>
+          </nav>
+          <div>
+              <h4 className='mb-4 text-nasa-red'>Social media</h4>
+              <ul className='space-y-2'>
                 <FooterItem name="Facebook" link="#"/>
                 <FooterItem name="LinkedIn" link="#"/>
                 <FooterItem name="Soundcloud" link="#"/>
                 <FooterItem name="" link="#"/>
-            </ul>
+              </ul>
+          </div>
         </div>
       </div>
     </footer>
--- src/components/footer/FooterItem.tsx
+++ src/components/footer/FooterItem.tsx
@@ -4,7 +4,7 @@ import { NavItemProps } from '../../types/types';
 export const FooterItem = (props: NavItemProps) => {
   return (
       <li>
-          <a href={props.link} className='hover:text-nasa-red'>
+          <a href={props.link} className='hover:text-nasa-red pl-1'>
               {props.name}
           </a>
       </li>
