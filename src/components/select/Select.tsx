8189cd714a8a8cbdff1b136e89505ae2344daaf0
KMininkov <mininkov3228@gmail.com>
2022-01-27 20:12:43 +0200

    Bug fixes - fixed elements under background blurs

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -59,7 +59,7 @@ export const Select = () => {
          before:right-[-15%] before:lg:right-[-1%] before:bottom-[5%] before:lg:bottom-[10%]
        before:from-fuchsia-600/70 before:via-cyan-600/30 before:to-indigo-600/70'>
         
-            <div className='container mx-auto'>
+            <div className='container mx-auto relative z-10'>
                 <h2 className='text-4xl'>Setup Photolist</h2>
 
                 <div className='my-16 flex gap-[2rem] flex-auto flex-wrap justify-between align-center lg:mr-16'>
